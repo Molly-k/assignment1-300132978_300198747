1129b5e0daa895ef58fce95dc1ee1e7e3aafcaf9
Molly-k <64807621+Molly-k@users.noreply.github.com>
2023-05-28 19:24:23 -0400

    Update PointCP2.java

--- pointcp/design2/PointCP2.java
+++ pointcp/design2/PointCP2.java
@@ -45,16 +45,12 @@ public class PointCP2
   public double getX()
   {
     if(typeCoord == 'C') 
-      return xOrRho;
-    else 
       return (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
   }
   
   public double getY()
   {
     if(typeCoord == 'C') 
-      return yOrTheta;
-    else 
       return (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
   }
   
@@ -62,16 +58,14 @@ public class PointCP2
   {
     if(typeCoord == 'P') 
       return xOrRho;
-    else 
-      return (Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2)));
+    
   }
   
   public double getTheta()
   {
     if(typeCoord == 'P')
       return yOrTheta;
-    else 
-      return Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
+   
   }
   
 	
